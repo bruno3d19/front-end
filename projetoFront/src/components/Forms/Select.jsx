d8eca62a1856acf834120387e58b8581b1434e50
bruno3d19 <brunorodriguesdosantos16@gmail.com>
2024-10-17 15:00:52 -0300

    ativ 3

--- projetoFront/src/components/Forms/Select.jsx
+++ projetoFront/src/components/Forms/Select.jsx
@@ -1,18 +1,23 @@
 import styles from './Select.module.css'
 
-function Select({name, text}) {
+function Select({name, text, options, handleOnChange, value}) {
     return(
         <div className={styles.form_control}>
 
             <label htmlFor={name}>{text}</label>
-            <select name={name} id={name}>
-                <option>HQS</option>
-                <option>MANGÁS</option>
-                <option>FIGURE</option>
-                <option>POSTERS</option>
-                <option>OUTROS</option>
-            </select>
+            <select 
+                // name={name} 
+                // id={name} 
+                // onChange={handleOnChange} 
+                // value={value || ''}
+                >
 
+                <option>Selecione uma opção</option>
+                {options.map((option) => {
+                    <option >{option.name}</option>
+                })}
+
+            </select>
         </div>
 
     )
--- projetoFront/src/components/pages/Cadastro.jsx
+++ projetoFront/src/components/pages/Cadastro.jsx
@@ -1,3 +1,5 @@
+import {useEffect, useState} from 'react'
+
 import NavBar from '../Layout/NavBar'
 import style from './Cadastro.module.css'
 
@@ -5,37 +7,123 @@ import Input from '../Forms/Input'
 import Select from '../Forms/Select' 
 import Button from '../Forms/Button' 
 
-function Cadastro() {
+const Cadastro = (handleSubmit, projectData) => {
+
+    const [categorias, setCategorias] = useState([])
+    const [project, setProject] =useState(projectData || {})
+
+    function handleChange(e) {
+        setProject({ ...project, [e.target.name]: e.target.value })
+    }
+
+    useEffect(() =>{
+        fetch("http://localhost:5000/categorias", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin':'*',
+                'Access-Control-Allow-Headers':'*',
+            },
+        })  
+            .then(
+                (resp) =>
+                     
+                    resp.json()
+                ).then(
+                    (data) =>{
+                        console.log('DATA: ' + data.data[3].categorias)
+                setCategorias(data.data)
+            })
+            .catch((err) => console.log(err))
+    }, []);
+
+    function createPost (project) {
+
+        console.log(JSON.stringify(project))
+
+        // project.cost = 0
+        // project.services = []
+
+        fetch("http://localhost:5000/categorias" ,{
+            method:'POST',
+            mode:'cors',
+            headers:{
+            'Content-Type':'application/json',
+            'Access-Control-Allow-Origin':'*',
+            'Access-Control-Allow-Headers':'*'
+            },
+            body: JSON.stringify(project)
+        })
+        .then(
+            (resp)=>resp.json()
+)
+.then(
+            (data)=>{
+            console.log(data);
+        // navigate('/livros',{state:'LIVRO CADASTRADO COM SUCESSO!'});
+        }
+)
+.catch(
+            (err)=>{ console.log(err) }
+)
+    
+    }
+
+    const submit = (e) => {
+        e.preventDefault();
+        //console.log(project)
+        createPost(project);
+        
+    }
+
+    // function handleCategory(e) {
+    //     setProject({ ...project, category: {
+    //         id: e.target.value,
+    //         name: e.target.options[e.target.selectedIndex].text,
+    //     },
+    //  })
+    // }
+
+
     return (
         
         <section>
 
-            <NavBar/>
+            {/* <NavBar/> */}
             <div className={style.cad_item}>
             <h1>CADASTRO</h1>
+
+            <form onSubmit={submit}>
 
             <Input
                 type='text'
                 name='txt_item'
-                placeHolder='Digite o nome do item aqui'
-                text='Titulo do item'
+                placeHolder='Digite o Titulo aqui'
+                text='Titulo:'
+                handleOnChange={handleChange}
             />
 
             <Input  
                 type='text'
                 name='txt_descricao_item'
-                placeHolder='Digite a descrição do item'
-                text='Descrição do item'
+                placeHolder='Digite a item aqui'
+                text='item:'
+                handleOnChange={handleChange}
             />
 
             <Select
-                name='categoria'
-                text='Escolha uma categoria de items'
+                name='categoria' 
+                text='Escolha uma categoria de items' 
+                options={categorias}
+                // handleOnChange={handleCategory}
+                // value={project.category ? project.category.id : ''}
             />
 
             <Button
                 rotulo='Cadastrar item'
+                // handleSubmit={createPost}
             />
+            </form>
 
             </div>
 
